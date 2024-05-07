2e1853439ad578eb25dcbc07787657fea02011f0
Moataz Mohamed <mohamed.moataz194@gmail.com>
2024-05-07 11:57:52 +0300

    models

--- lib/features/home/domain/entities/test_model.dart
+++ lib/features/home/domain/entities/test_model.dart
@@ -3,18 +3,37 @@ import 'package:json_annotation/json_annotation.dart';
 part 'test_model.g.dart';
 
 @JsonSerializable(explicitToJson: true, fieldRename: FieldRename.snake)
-class TestModel extends Equatable{
+class TestModel extends Equatable {
   int? id;
   int? programId;
   int? mistakeCount;
-  int? status;
+  dynamic status;
+  int? testId;
   String? name;
+  String? testName;
+  String? teacherName;
+  String? type;
+  String? formattedDueDate;
+  String? daysLeft;
   String? duration;
   String? createdAt;
   String? updatedAt;
 
-  TestModel({this.id, this.name, this.createdAt, this.updatedAt, this.duration, this.mistakeCount, this.programId, this.status});
-
+  TestModel(
+      {this.id,
+      this.name,
+      this.createdAt,
+      this.updatedAt,
+      this.duration,
+      this.mistakeCount,
+      this.programId,
+      this.status,
+      this.testName,
+      this.daysLeft,
+      this.formattedDueDate,
+      this.teacherName,
+      this.testId,
+      this.type});
 
   factory TestModel.fromJson(Map<String, dynamic> json) {
     return _$TestModelFromJson(json);
@@ -23,5 +42,19 @@ class TestModel extends Equatable{
   Map<String, dynamic> toJson() => _$TestModelToJson(this);
 
   @override
-  List<Object?> get props => [id, name, createdAt, duration, mistakeCount, programId, status];
+  List<Object?> get props => [
+        id,
+        name,
+        createdAt,
+        duration,
+        mistakeCount,
+        programId,
+        status,
+        testId,
+        testName,
+        teacherName,
+        type,
+        formattedDueDate,
+        daysLeft
+      ];
 }
--- lib/features/home/domain/entities/test_model.g.dart
+++ lib/features/home/domain/entities/test_model.g.dart
@@ -14,7 +14,13 @@ TestModel _$TestModelFromJson(Map<String, dynamic> json) => TestModel(
       duration: json['duration'] as String?,
       mistakeCount: (json['mistake_count'] as num?)?.toInt(),
       programId: (json['program_id'] as num?)?.toInt(),
-      status: (json['status'] as num?)?.toInt(),
+      status: json['status'],
+      testName: json['test_name'] as String?,
+      daysLeft: json['days_left'] as String?,
+      formattedDueDate: json['formatted_due_date'] as String?,
+      teacherName: json['teacher_name'] as String?,
+      testId: (json['test_id'] as num?)?.toInt(),
+      type: json['type'] as String?,
     );
 
 Map<String, dynamic> _$TestModelToJson(TestModel instance) => <String, dynamic>{
@@ -22,7 +28,13 @@ Map<String, dynamic> _$TestModelToJson(TestModel instance) => <String, dynamic>{
       'program_id': instance.programId,
       'mistake_count': instance.mistakeCount,
       'status': instance.status,
+      'test_id': instance.testId,
       'name': instance.name,
+      'test_name': instance.testName,
+      'teacher_name': instance.teacherName,
+      'type': instance.type,
+      'formatted_due_date': instance.formattedDueDate,
+      'days_left': instance.daysLeft,
       'duration': instance.duration,
       'created_at': instance.createdAt,
       'updated_at': instance.updatedAt,
