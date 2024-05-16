e8744c21d039dd6501bf473db388ce6321f1acb3
Moataz Mohamed <mohamed.moataz194@gmail.com>
2024-05-16 13:57:13 +0300

    change riv files

--- lib/features/student_assignment/presentation/pages/get_assignment.dart
+++ lib/features/student_assignment/presentation/pages/get_assignment.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+  import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:mind_buzz_refactor/core/assets_images.dart';
