1e7c6002d5dc628bdc2cd8071ecb6cd5a3176b62
Moataz Mohamed <mohamed.moataz194@gmail.com>
2024-05-22 10:11:30 +0300

    fixed bug in drag out

--- lib/features/phonetics/presentation/games/drag_out/manager/drag_out_cubit.dart
+++ lib/features/phonetics/presentation/games/drag_out/manager/drag_out_cubit.dart
@@ -9,9 +9,32 @@ import '../../../../domain/entities/game_model.dart';
 part 'drag_out_state.dart';
 
 class DragOutCubit extends Cubit<DragOutInitial> {
-  final GameModel gameData;
+  final List<GameModel> gameData;
   DragOutCubit({required this.gameData})
       : super(DragOutInitial(gameData: gameData)) {
-    TalkTts.startTalk(text: gameData.mainLetter ?? '');
+    // TalkTts.startTalk(text: gameData.mainLetter ?? '');
+  }
+
+  Future<int>increaseCountOfCorrectAnswers()async{
+    int sub = state.correctAnswer??0;
+    sub = sub +1;
+    emit(state.copyWith(correctAnswer: sub));
+    return Future.value(state.correctAnswer);
+  }
+
+  bool checkIfIsTheLastGameOfLesson() {
+    int currentIndex = state.index??0;
+    currentIndex = currentIndex + 1;
+    if ((state.gameData.length ) > currentIndex) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  updateIndexOfCurrentGame() {
+    int currentIndex = state.index??0;
+    currentIndex = currentIndex + 1;
+    emit(state.copyWith(index: currentIndex));
   }
 }
--- lib/features/phonetics/presentation/games/drag_out/pages/drag_out_game.dart
+++ lib/features/phonetics/presentation/games/drag_out/pages/drag_out_game.dart
@@ -15,133 +15,182 @@ import '../../../manager/main_cubit/current_game_phonetics_cubit.dart';
 import '../manager/drag_out_cubit.dart';
 
 class DragOutGame extends StatelessWidget {
-  final GameModel gameData;
-  DragOutGame({required this.gameData});
+  // final List<GameModel> gameData;
+  // DragOutGame({required this.gameData});
   @override
   Widget build(BuildContext context) {
     // final gameData = context.watch<DragOutCubit>().state.gameData;
     final stateOfCurrentGamePhoneticsCubit =
         context.watch<CurrentGamePhoneticsCubit>().state;
     return Container(
-      alignment: Alignment.center,
-      // height: MediaQuery.of(context).size.height - (70.h),
-      child: Row(
-        // mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Container(
-              margin: const EdgeInsets.only(bottom: 15, left: 10),
-              child: DragTarget<GameImagesModel>(builder: (
-                BuildContext context,
-                List<dynamic> accepted,
-                List<dynamic> rejected,
-              ) {
-                return (stateOfCurrentGamePhoneticsCubit.stateOfAvatar ==
-                        BasicOfEveryGame.stateOfWin)
-                    ? Image.asset(
-                        stateOfCurrentGamePhoneticsCubit
-                                .basicData?.gameData?.completeBasket ??
-                            '',
-                        height: (MediaQuery.of(context).size.height / 2.8).h,
-                        width: 130,
-                      )
-                    : Image.asset(
-                        AppImagesPhonetics.imageBasket,
-                        height: (MediaQuery.of(context).size.height / 2.8).h,
-                        width: 130,
-                      );
-              }, onAcceptWithDetails: (item) {
-                if (item.data.word.toString().split('').first.toLowerCase() !=
-                    (gameData.mainLetter?.toLowerCase() ?? '')) {
-                  context.read<CurrentGamePhoneticsCubit>().addSuccessAnswer(
-                      actionInEndOfLesson: () {
-                    Navigator.of(context).pop();
-                  });
-                }
-                else {
-                  context.read<CurrentGamePhoneticsCubit>().addWrongAnswer(actionOfWrongAnswer: (){
-                    // TalkTts.startTalk(text: gameData.mainLetter ?? '');
+        alignment: Alignment.center,
+        // height: MediaQuery.of(context).size.height - (70.h),
+        child: BlocConsumer<DragOutCubit, DragOutInitial>(
+            listener: (context, state) {},
+            builder: (context, gameState) {
+              return Row(
+                // mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                      margin: const EdgeInsets.only(bottom: 15, left: 10),
+                      child: DragTarget<GameImagesModel>(builder: (
+                        BuildContext context,
+                        List<dynamic> accepted,
+                        List<dynamic> rejected,
+                      ) {
+                        return (stateOfCurrentGamePhoneticsCubit
+                                    .stateOfAvatar ==
+                                BasicOfEveryGame.stateOfWin)
+                            ? Image.asset(
+                                stateOfCurrentGamePhoneticsCubit
+                                        .basicData?.gameData?.completeBasket ??
+                                    '',
+                                height:
+                                    (MediaQuery.of(context).size.height / 2.8)
+                                        .h,
+                                width: 130,
+                              )
+                            : Image.asset(
+                                AppImagesPhonetics.imageBasket,
+                                height:
+                                    (MediaQuery.of(context).size.height / 2.8)
+                                        .h,
+                                width: 130,
+                              );
+                      }, onAcceptWithDetails: (item) async {
+                        if (item.data.word
+                                .toString()
+                                .split('')
+                                .first
+                                .toLowerCase() !=
+                            (gameState.gameData[gameState.index??0].mainLetter?.toLowerCase() ?? '')) {
+                          await context
+                              .read<CurrentGamePhoneticsCubit>()
+                              .animationOfCorrectAnswer();
+                          int countOfCorrect = await context
+                              .read<DragOutCubit>()
+                              .increaseCountOfCorrectAnswers();
+                          context
+                              .read<CurrentGamePhoneticsCubit>()
+                              .addStarToStudent(
+                            stateOfCountOfCorrectAnswer:
+                            countOfCorrect,
+                            mainCountOfQuestion: gameState.gameData.length,
+                          );
+                          bool isLastLesson = context
+                              .read<DragOutCubit>()
+                              .checkIfIsTheLastGameOfLesson();
+                          if (isLastLesson == true) {
+                            await Future.delayed(const Duration(seconds: 2));
+                            Navigator.of(context).pop();
 
-                  });
-                }
-              })),
-          Container(
-            margin: const EdgeInsets.only(bottom: (15 + 50), top: 50),
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-            width: MediaQuery.of(context).size.width - (130 + 50 + 130),
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(15),
-                border: Border.all(
-                    color: AppColorPhonetics.boarderColor, width: 5)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: List.generate(
-                  gameData.gameImages?.length ?? 0,
-                  (index) => Draggable<GameImagesModel>(
-                      data: gameData.gameImages?[index],
-                      childWhenDragging: SizedBox(
-                        width: (MediaQuery.of(context).size.width -
-                                (130 + 50 + 130)) /
-                            4,
-                        height: 130.h,
-                      ),
-                      feedback: CachedNetworkImage(
-                        imageUrl: gameData.gameImages?[index].image ?? '',
-                        width: (MediaQuery.of(context).size.width -
-                                (130 + 50 + 130)) /
-                            4,
-                        height: 130.h,
-                        placeholder: (context, url) => const Center(
-                          child: CupertinoActivityIndicator(),
-                        ),
-                        errorWidget: (context, url, error) => const Icon(
-                          Icons.error,
-                          color: Colors.red,
-                        ),
-                        // height: ,
-                      ),
-                      child: ((stateOfCurrentGamePhoneticsCubit.stateOfAvatar ==
-                                  BasicOfEveryGame.stateOfWin) &&
-                              (gameData.gameImages?[index].word
-                                      .toString()
-                                      .split('')
-                                      .first
-                                      .toLowerCase() !=
-                                  (gameData.mainLetter?.toLowerCase() ?? '')))
-                          ? SizedBox(
-                              width: (MediaQuery.of(context).size.width -
-                                      (130 + 50 + 130)) /
-                                  4,
-                              height: 130.h,
-                              // height: ,
-                            )
-                          : GestureDetector(
-                        onTap: (){
-                          TalkTts.startTalk(text: gameData.gameImages?[index].word ?? '');
-                        },
-                            child: CachedNetworkImage(
-                                imageUrl: gameData.gameImages?[index].image ?? '',
+                          } else {
+                            await context
+                                .read<CurrentGamePhoneticsCubit>()
+                                .backToMainAvatar();
+                            context
+                                .read<DragOutCubit>()
+                                .updateIndexOfCurrentGame();
+                          }
+                        } else {
+                          context
+                              .read<CurrentGamePhoneticsCubit>()
+                              .addWrongAnswer(actionOfWrongAnswer: () {
+                            // TalkTts.startTalk(text: gameData.mainLetter ?? '');
+                          });
+                        }
+                      })),
+                  Container(
+                    margin: const EdgeInsets.only(bottom: (15 + 50), top: 50),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 20),
+                    width: MediaQuery.of(context).size.width - (130 + 50 + 130),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(15),
+                        border: Border.all(
+                            color: AppColorPhonetics.boarderColor, width: 5)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: List.generate(
+                          gameState.gameData[gameState.index??0].gameImages?.length ?? 0,
+                          (index) => Draggable<GameImagesModel>(
+                              data: gameState.gameData[gameState.index??0].gameImages?[index],
+                              childWhenDragging: SizedBox(
                                 width: (MediaQuery.of(context).size.width -
                                         (130 + 50 + 130)) /
                                     4,
                                 height: 130.h,
-                                placeholder: (context, url) => Center(
-                                  child: CircularProgressIndicator(),
+                              ),
+                              feedback: CachedNetworkImage(
+                                imageUrl:
+                                gameState.gameData[gameState.index??0].gameImages?[index].image ?? '',
+                                width: (MediaQuery.of(context).size.width -
+                                        (130 + 50 + 130)) /
+                                    4,
+                                height: 130.h,
+                                placeholder: (context, url) => const Center(
+                                  child: CupertinoActivityIndicator(),
                                 ),
-                                errorWidget: (context, url, error) => Icon(
+                                errorWidget: (context, url, error) =>
+                                    const Icon(
                                   Icons.error,
                                   color: Colors.red,
                                 ),
                                 // height: ,
                               ),
-                          ))),
-            ),
-          )
-        ],
-      ),
-    );
+                              child: ((stateOfCurrentGamePhoneticsCubit
+                                              .stateOfAvatar ==
+                                          BasicOfEveryGame.stateOfWin) &&
+                                      (gameState.gameData[gameState.index??0].gameImages?[index].word
+                                              .toString()
+                                              .split('')
+                                              .first
+                                              .toLowerCase() !=
+                                          (gameState.gameData[gameState.index??0].mainLetter?.toLowerCase() ??
+                                              '')))
+                                  ? SizedBox(
+                                      width:
+                                          (MediaQuery.of(context).size.width -
+                                                  (130 + 50 + 130)) /
+                                              4,
+                                      height: 130.h,
+                                      // height: ,
+                                    )
+                                  : GestureDetector(
+                                      onTap: () {
+                                        TalkTts.startTalk(
+                                            text: gameState.gameData[gameState.index??0]
+                                                    .gameImages?[index].word ??
+                                                '');
+                                      },
+                                      child: CachedNetworkImage(
+                                        imageUrl:
+                                        gameState.gameData[gameState.index??0].gameImages?[index].image ??
+                                                '',
+                                        width:
+                                            (MediaQuery.of(context).size.width -
+                                                    (130 + 50 + 130)) /
+                                                4,
+                                        height: 130.h,
+                                        placeholder: (context, url) => Center(
+                                          child: CircularProgressIndicator(),
+                                        ),
+                                        errorWidget: (context, url, error) =>
+                                            Icon(
+                                          Icons.error,
+                                          color: Colors.red,
+                                        ),
+                                        // height: ,
+                                      ),
+                                    ))),
+                    ),
+                  )
+                ],
+              );
+            }));
   }
 }
