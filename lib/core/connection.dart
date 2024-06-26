ac3f91220bbd33361f123e263d053beb8f052427
moatazmohamed <mohamed.moataz194@gmail.com>
2024-06-27 00:12:08 +0300

    change the base of url

--- lib/core/connection.dart
+++ lib/core/connection.dart
@@ -3,27 +3,26 @@ import 'assets_sound.dart';
 abstract class Connection {
   Connection._();
 
-  static const base = 'ambernoak.co.uk/Fillament/public';
+  static const base = 'hqsync.duckdns.org/Fillament/public';
   static const baseURL = 'https://$base/api/';
 }
 
-abstract class QuestionType{
-  static const howManyMore= 'How Many more?';
-  static const count= 'Count';
-  static const sum= 'Sum';
+abstract class QuestionType {
+  static const howManyMore = 'How Many more?';
+  static const count = 'Count';
+  static const sum = 'Sum';
 }
 
-abstract class Type{
-  static const dragDrop= 'Drag & Drop';
-  static const color= 'Color';
-  static const mCQ= 'MCQ';
-  static const matching= 'Matching';
+abstract class Type {
+  static const dragDrop = 'Drag & Drop';
+  static const color = 'Color';
+  static const mCQ = 'MCQ';
+  static const matching = 'Matching';
 }
 
-abstract class SecType{
-  static const rods= 'Rods';
-  static const beads= 'Beads';
-  static const stripBoard= 'Strip Board';
-  static const chart= 'Chart';
+abstract class SecType {
+  static const rods = 'Rods';
+  static const beads = 'Beads';
+  static const stripBoard = 'Strip Board';
+  static const chart = 'Chart';
 }
-
